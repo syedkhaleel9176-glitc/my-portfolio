71636097b799c22b86d2854ee388ef3e3c5b556d
Syed Khaleel <syedkhaleel9176@gmail.com>
2026-03-26 10:05:42 +0530

    change resume

--- src/components/Pdf.jsx
+++ src/components/Pdf.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './Resume.css'
-import resumefile from "../assets/resume/Syed_khaleel.pdf"
+import resumefile from "../assets/resume/Syed_Khaleel.pdf"
 
 const Pdf = () => {
   return (
     <div>
       <div className="down">
       <h1> Download The Resume</h1><br/>
-      <a href={resumefile} download="syed khaleel Resume">
+      <a href={resumefile} download="syed Resume">
         <button type="button" className="btn btn-danger"><i className="bi bi-cloud-arrow-down-fill">Download pdf</i></button>
       </a>
       </div>
