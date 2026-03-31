c7a5c0cc03d967c09955382b69820381460a6f04
Syed Khaleel <syedkhaleel9176@gmail.com>
2026-03-31 14:42:29 +0530

    Resume update

--- src/components/Pdf.jsx
+++ src/components/Pdf.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Resume.css'
-import resumefile from "../assets/resume/Syed khaleel S.pdf"
+import resumefile from "../assets/resume/Syed khaleel.pdf"
 
 const Pdf = () => {
   return (
